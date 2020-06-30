c74d72c91bf1559f6abfb455083f40b427762339
DeveshChau <deveshachaudhari@gmail.com>
2020-06-30 09:53:42 +0530

    book list set up

--- models/author.js
+++ models/author.js
@@ -1,40 +1,48 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const AutherSchema = new Schema({
-    first_name: {type: String, required: true, maxlength:100},
-    family_name: {type: String, required: true, maxlength:100},
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var AuthorSchema = new Schema(
+  {
+    first_name: {type: String, required: true, maxlength: 100},
+    family_name: {type: String, required: true, maxlength: 100},
     date_of_birth: {type: Date},
-    date_of_death: {type: Date}
-})
+    date_of_death: {type: Date},
+  }
+);
 
-//virtuals for writer full name
-AutherSchema.virtual('name')
-.get(() => {
-    let fullName = '';
-    if (this.first_name && this.family_name) {
-        fullName = `${this.first_name}, ${this.family_name}`;
-    }
+// Virtual for author's full name
+AuthorSchema
+.virtual('name')
+.get(function () {
 
-    if (!this.first_name || !this.family_name) {
-        fullName = '';
-    }
+// To avoid errors in cases where an author does not have either a family name or first name
+// We want to make sure we handle the exception by returning an empty string for that case
 
-    return fullName;
-})
+  var fullname = '';
+  if (this.first_name && this.family_name) {
+    fullname = this.family_name + ', ' + this.first_name
+  }
+  if (!this.first_name || !this.family_name) {
+    fullname = '';
+  }
 
-// Virtuals for writer lifespan
-AutherSchema.virtual('lifespan')
-.get(() => {
-    return (this.date_of_death.getYear() - this.date_of_birth.getYear()).toString();
-})
+  return fullname;
+});
 
+// Virtual for author's lifespan
+AuthorSchema
+.virtual('lifespan')
+.get(function () {
+  return (this.date_of_death.getYear() - this.date_of_birth.getYear()).toString();
+});
 
 // Virtual for author's URL
-AutherSchema
+AuthorSchema
 .virtual('url')
 .get(function () {
   return '/catalog/author/' + this._id;
 });
 
 //Export model
-module.exports = mongoose.model('Author', AutherSchema);
+module.exports = mongoose.model('Author', AuthorSchema);
